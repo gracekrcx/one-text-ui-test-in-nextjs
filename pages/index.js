e84014e782a695fcddeee6f619a286d3d8bbb5c6
Grace <kanmorning@gmail.com>
2024-02-19 17:57:26 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,26 @@
 import styles from '../styles/Home.module.css';
 import { useState } from 'react';
 
-function renderAlert(showError, productLimitBuyText, maximumOfSinglePurchase) {
+function renderAlert(showError, limitBuyText, maximum) {
   if (showError) {
-    return productLimitBuyText
-      ? `( 已超出${productLimitBuyText} )`
-      : `已超出購買數量，上限${maximumOfSinglePurchase}`;
+    return limitBuyText
+      ? `( 已超出${limitBuyText} )`
+      : `已超出購買數量，上限${maximum}`;
   }
-  if (productLimitBuyText) {
-    return `( ${productLimitBuyText} )`;
+  if (limitBuyText) {
+    return `( ${limitBuyText} )`;
   }
   return '';
 }
 
 export default function Home() {
   const [showError, setShowError] = useState(false);
-  const [productLimitBuyText, setProductLimitBuyText] = useState('限購 5 件'); // 限購 5 件
-  const [maximumOfSinglePurchase, setMaximumOfSinglePurchase] = useState(5);
+  const [limitBuyText, setLimitBuyText] = useState('限購 5 件'); // 限購 5 件
+  const [maximum, setMaximum] = useState(5);
 
   return (
     <div className={styles.container}>
-      {renderAlert(showError, productLimitBuyText, maximumOfSinglePurchase)}
+      {renderAlert(showError, limitBuyText, maximum)}
     </div>
   );
 }
